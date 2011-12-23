5a549eb9314ba6c4af1307af650d7b91f2d82574
Douglas Tarr <douglas.tarr@gmail.com>
2011-12-22 22:29:21 -0800

    User database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Todo::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
   resources :todo_files
@@ -52,7 +54,7 @@ Todo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#:index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

4ed027092db587101cd0e4bff8c234855f9789c5
Douglas Tarr <douglas.tarr@gmail.com>
2011-12-25 15:38:58 -0800

    Move tasks to the home page so you can see them logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
-class HomeController < ApplicationController
-  def index
 
+class HomeController < ApplicationController
+   before_filter :authenticate_user! 
+  def index
+    @tasks = current_user.tasks.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tasks }
+    end
+ 
+  
+  
   end
 
 end

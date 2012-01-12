d2a0e9afaef9954fda96dc31ab7d9e8ffc7ec206
Douglas Tarr <douglas.tarr@gmail.com>
2012-01-11 16:41:09 -0800

    redirect home page to explorer view precompile assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,35 +3,36 @@ class HomeController < ApplicationController
    before_filter :authenticate_user! 
   def index
 
-    topfolders = current_user.task_folder.task_folders
-
-    @topfolders = current_user.task_folder.task_folders.push(current_user.task_folder)
-    @topfolders.sort_by!{|a| a.path.downcase}
-
-    @cols = @topfolders.length - 1
-    @rows = 0
-
-    @matrix = Hash.new
-    @topfolders.each_with_index do |folder, col|
-      folder.all.each_with_index do |item, row|
-        if (col == 0)
-          next
-        end
-        @matrix[[col, row]] = item
-        if row > @rows
-          @rows = row
-        end
-      end
-    end
-
-    # to get the # of rows, we have to find th
-
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @files}
-    end
- 
+    redirect_to :controller=>"task_folder", :action => "folder_view", :path => "/"
+    #topfolders = current_user.task_folder.task_folders
+    #
+    #@topfolders = current_user.task_folder.task_folders.push(current_user.task_folder)
+    #@topfolders.sort_by!{|a| a.path.downcase}
+    #
+    #@cols = @topfolders.length - 1
+    #@rows = 0
+    #
+    #@matrix = Hash.new
+    #@topfolders.each_with_index do |folder, col|
+    #  folder.all.each_with_index do |item, row|
+    #    if (col == 0)
+    #      next
+    #    end
+    #    @matrix[[col, row]] = item
+    #    if row > @rows
+    #      @rows = row
+    #    end
+    #  end
+    #end
+    #
+    ## to get the # of rows, we have to find th
+    #
+    #
+    #respond_to do |format|
+    #  format.html # index.html.erb
+    #  format.json { render json: @files}
+    #end
+    #
   
   
   end

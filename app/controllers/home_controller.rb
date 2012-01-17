be18c223098f4fa88d25b9b41b34ea20538ceed7
Douglas Tarr <douglas.tarr@gmail.com>
2012-01-17 15:07:28 -0800

    mobile attempts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,19 +2,19 @@ class HomeController < ApplicationController
    before_filter :authenticate_user!, :except => :index
    respond_to_mobile_requests :skip_xhr_requests => false
 
-  def settings
-    @user = current_user
-    respond_to do |format|
-      format.html # index.html.erb
-      format.mobile
-    end
-  end
 
   def index
 
     if user_signed_in?
      redirect_to :controller=>"task_folder", :action => "folder_view", :path => "/"
+    else
+      respond_to do |format|
+        format.html # index.html.erb
+        format.mobile
+      end
+
     end
+
 
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < Devise::SessionsController
 
   before_filter :force_ssl, :only => :new
+  #@respond_to_mobile_requests :skip_xhr_requests => false
 
   private
 

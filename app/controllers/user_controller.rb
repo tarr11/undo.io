e3a828e0b683422be1906975a48dee33c5c9cabb
Douglas Tarr <douglas.tarr@gmail.com>
2012-01-17 12:59:00 -0800

    respond to mobile on settings page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,9 +13,11 @@ class UserController < ApplicationController
     respond_to do |format|
       if current_user.update_attributes(params[:user])
         format.html { redirect_to :action=>:show, notice: 'Settings were successfully updated.' }
+        format.mobile
         format.json { head :ok }
       else
         format.html { render action: "edit" }
+        format.mobile
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end

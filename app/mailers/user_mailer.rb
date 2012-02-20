5770166c4fcc54da9350916f6a3d89171a59e8d2
Douglas Tarr <douglas.tarr@gmail.com>
2012-02-20 09:43:50 -1000

    fix email configuration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,6 @@
 class UserMailer < ActionMailer::Base
 
-  default :from => "notes@undo.io"
-  default_url_options[:host] = "undo.io"
-   
+
     def daily_email(user, summary)
       @user = user
       @summary = summary

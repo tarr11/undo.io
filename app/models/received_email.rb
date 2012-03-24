5f360efa73feb258973fc65c748c9138d35adb9a
Douglas Tarr <douglas.tarr@gmail.com>
2012-03-24 13:01:04 -0700

    strip chars

--- app/models/received_email.rb
+++ app/models/received_email.rb
@@ -40,7 +40,7 @@ class ReceivedEmail
     # strip email header
     lines = []
     @body_plain.split("\n").each do |line|
-      lines.push line.gsub(/^>+/,"")
+      lines.push line.gsub(/^>+/,"").lstrip
     end
    # find the footer, and chop that off.  it is the first line from the bottom starting with "--"
 

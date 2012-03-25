d9e5429a2517ada5f7d1d2f799c7034e42b883c4
Douglas Tarr <douglas.tarr@gmail.com>
2012-03-24 19:03:22 -0700

    fixed reply parsing

--- app/models/received_email.rb
+++ app/models/received_email.rb
@@ -28,7 +28,7 @@ class ReceivedEmail
   end
 
   def reply_to_id
-    match = self.body_plain.match(/reply_to_id:(?<id>[^\b]+)\b/)
+    match = self.body_plain.match(/reply_to_id:(?<id>[^\b]+)$/)
     unless match.nil?
       return match["id"]
     end

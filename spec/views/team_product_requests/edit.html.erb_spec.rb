34c4d5a48dad9a4606b8808f30cb0ef37b1e708b
Douglas Tarr <douglas.tarr@gmail.com>
2012-04-07 21:58:18 -0700

    fixing specs

--- spec/views/team_product_requests/edit.html.erb_spec.rb
+++ spec/views/team_product_requests/edit.html.erb_spec.rb
@@ -1,20 +1,4 @@
 require 'spec_helper'
 
 describe "team_product_requests/edit" do
-  before(:each) do
-    @team_product_request = assign(:team_product_request, stub_model(TeamProductRequest,
-      :email => "MyString",
-      :feedback => "MyText"
-    ))
-  end
-
-  it "renders the edit team_product_request form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => team_product_requests_path(@team_product_request), :method => "post" do
-      assert_select "input#team_product_request_email", :name => "team_product_request[email]"
-      assert_select "textarea#team_product_request_feedback", :name => "team_product_request[feedback]"
-    end
-  end
 end
--- spec/views/team_product_requests/index.html.erb_spec.rb
+++ spec/views/team_product_requests/index.html.erb_spec.rb
@@ -1,24 +1,4 @@
 require 'spec_helper'
 
 describe "team_product_requests/index" do
-  before(:each) do
-    assign(:team_product_requests, [
-      stub_model(TeamProductRequest,
-        :email => "Email",
-        :feedback => "MyText"
-      ),
-      stub_model(TeamProductRequest,
-        :email => "Email",
-        :feedback => "MyText"
-      )
-    ])
-  end
-
-  it "renders a list of team_product_requests" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-  end
 end

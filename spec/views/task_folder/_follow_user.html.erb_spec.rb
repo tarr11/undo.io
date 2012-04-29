ef1e4b5e6575a194174b23fbc54d3a8b8f2c28d5
Douglas Tarr <douglas.tarr@gmail.com>
2012-04-29 11:57:32 -0700

    when public logged out user pages threw NRE

--- spec/views/task_folder/_follow_user.html.erb_spec.rb
+++ spec/views/task_folder/_follow_user.html.erb_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 include TaskFolderHelper
 
-describe "task_folder/folder_view" do
+describe "task_folder/_follow_user" do
   
   describe 'when not logged in' do
     before (:each) do
-      user = Factory.create(:user)
-      file = user.todo_files.create(Factory.attributes_for(:file))
+      @user = Factory.create(:user)
+      file = @user.todo_files.create(Factory.attributes_for(:file))
       @views = []
       @changed_files_by_date = get_changed_files_by_date([file])
       view.stub!(:user_owns_file).and_return(true)
     end
 
     it "should be successful" do
+      assign(:user, @user)
       render
     end
   end
@@ -20,15 +21,17 @@ describe "task_folder/folder_view" do
   describe 'when logged in' do
     login_user
     before (:each) do
-      user = view.current_user
-      file = user.todo_files.create(Factory.attributes_for(:file))
+      @user = Factory.create(:user2)
+      file = @user.todo_files.create(Factory.attributes_for(:file))
       @views = []
       @changed_files_by_date = get_changed_files_by_date([file])
       view.stub!(:user_owns_file).and_return(true)
+      view.stub!(:user).and_return(@user)
     end
 
     it "should be successful" do
       render
     end
   end
+
 end
